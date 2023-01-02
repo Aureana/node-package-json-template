ba17e6395fbb6cbc7628afd316578306c14227c8
Aureana <aureana2018@outlook.com>
2023-01-02 15:17:25 -0300

    Adicionando atividade fixação

---
+++ atividadeFix.js
@@ -0,0 +1,9 @@
+console.log("Fix")
+
+const nome = process.argv[2]
+const idade = Number(process.argv[3])
+
+console.log(`Olá ${nome}, você tem ${idade} anos`)
+
+//adicionar 7 ano
+console.log(`Olá ${nome}, daqui à sete anos, você terá ${idade+7} anos`)
